33daecc06727b2dae70576437019639457454a11
martin <martin@localhost>
2016-07-04 11:31:19 +0100

    Initial commit

---
+++ django_envar/__info__.py
@@ -0,0 +1,9 @@
+"""
+Project information.
+We use this module as __init__ is a generic module that django requires to be
+specifically set up.
+"""
+LABELS = dict()
+LABELS['class'] = 'DjangoEnvar'
+LABELS['name'] = 'django_envar'
+LABELS['verbose_name'] = 'Django Centralised Environment Variables Server'
